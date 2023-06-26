2782c89817068396d7698ac8f0e955b0573c392a
Ali Murtaza <112254545+CodewithAli45@users.noreply.github.com>
2023-06-26 23:37:27 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -3,31 +3,33 @@ var mongoose = require("mongoose");
 //Complete Schema so that while ading new product it should be valid.
 // No two products can have same name in Document .
 var productSchema = mongoose.Schema({
-    name : {
-        type : String,
-        required : true,
-        validate: {
-            async validator(value){
-                const nameExists = await checkName(User, value);
-                return !nameExists;
-            },
-            message: 'Product Name already exists',
-        }
+    name: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      isAsync: true,
+      validator: async function(value) {
+        const product = await this.constructor.findOne({ name: value });
+        return !product;
+      },
+      message: 'Product Name already exists',
     },
-    description : {
-        type : String, 
-        required: true,
-    },
-    Price: {
-        type : Number, 
-        required: true
-    }
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
   });
 
-  async function checkName(User, name){
-    const count = await User.countDocuments({name});
-    return count !== 0;
-  }
+  // async function checkName(User, name){
+  //   const count = await User.countDocuments({name});
+  //   return count !== 0;
+  // }
   
   
   
