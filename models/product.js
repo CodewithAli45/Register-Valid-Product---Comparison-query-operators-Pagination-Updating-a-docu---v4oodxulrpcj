e8bb248f50159f24130dc7e3a308c05d922325a8
Ali Murtaza <112254545+CodewithAli45@users.noreply.github.com>
2023-06-26 11:36:17 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -3,7 +3,9 @@ var mongoose = require("mongoose");
 //Complete Schema so that while ading new product it should be valid.
 // No two products can have same name in Document .
 var productSchema = mongoose.Schema({
-  
+  name : {type : String, unique: true, required: true},
+  description : {type : String, required: true},
+  Price: {type : Number, required: true}
 });
 
 
